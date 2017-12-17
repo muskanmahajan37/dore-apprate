4a33f5fcb811b3376d44b8103718fd4d9d633e35
Phodal HUANG <h@phodal.com>
2017-12-17 23:16:33 +0800

    [T] import code

--- ios/DoreAppRate/DoreAppRate.h
+++ ios/DoreAppRate/DoreAppRate.h
@@ -5,8 +5,23 @@
 //  Created by phodal on 17/12/2017.
 //
 
-#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <UIKit/UIKit.h>
+#import <React/RCTUtils.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <StoreKit/StoreKit.h>
 
-@interface DoreAppRate : NSObject
+@interface DoreAppRate : RCTEventEmitter <RCTBridgeModule>
+
+- (void)getAppVersion:(CDVInvokedUrlCommand *)command;
+
+- (void)getAppTitle:(CDVInvokedUrlCommand *)command;
+
+- (void)launchiOSReview:(CDVInvokedUrlCommand *)command;
+
+- (void)launchAppStore:(NSString *)appId;
+
+- (void)launchInAppReview;
 
 @end
--- ios/DoreAppRate/DoreAppRate.m
+++ ios/DoreAppRate/DoreAppRate.m
@@ -5,8 +5,83 @@
 //  Created by phodal on 17/12/2017.
 //
 
-#import "DoreAppRate.h"
+#import "RNAppRate.h"
+#import <React/RCTBridge.h>
+#import <StoreKit/StoreKit.h>
 
-@implementation DoreAppRate
+@implementation RNNoTagDatepicker
+
+@synthesize bridge = _bridge;
+
+- (NSArray<NSString *> *) supportedEvents
+{
+    return @[@"DATEPICKER_NATIVE_INVOKE"];
+}
+
+RCT_EXPORT_MODULE()
+
+- (void)getAppVersion:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        NSString *versionString = [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"];
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:versionString];
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)getAppTitle:(CDVInvokedUrlCommand *)command {
+    [self.commandDelegate runInBackground:^{
+        NSString *appNameString = [[NSBundle mainBundle] infoDictionary][@"CFBundleDisplayName"];
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:appNameString];
+        
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)launchiOSReview:(CDVInvokedUrlCommand *)command {
+    BOOL shouldUseNativePrompt = [command.arguments[1] boolValue];
+    
+    if (shouldUseNativePrompt && [SKStoreReviewController class]) {
+        [self launchInAppReview];
+    } else {
+        NSString *appId = @"";
+        
+        if ([command.arguments count] >= 1) {
+            appId = (NSString *) (command.arguments)[0];
+        }
+        
+        [self launchAppStore:appId];
+    }
+}
+
+- (void)launchAppStore:(NSString *) appId{
+    [self.commandDelegate runInBackground:^{
+        // Initialize Product View Controller
+        SKStoreProductViewController *storeProductViewController = [[SKStoreProductViewController alloc] init];
+        
+        // Configure View Controller
+        [storeProductViewController setDelegate:self];
+        [storeProductViewController loadProductWithParameters:@{SKStoreProductParameterITunesItemIdentifier : appId} completionBlock:^(BOOL result, NSError *error) {
+            if (error) {
+                NSLog(@"Error %@ with User Info %@.", error, [error userInfo]);
+            } else {
+                [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+                [self.viewController presentViewController:storeProductViewController animated:YES completion:nil];
+            }
+        }];
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    }];
+}
+
+- (void)launchInAppReview {
+    [self.commandDelegate runInBackground:^{
+        [SKStoreReviewController requestReview];
+    }];
+}
+
+- (void)productViewControllerDidFinish:(SKStoreProductViewController *)viewController {
+    [viewController dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
+
