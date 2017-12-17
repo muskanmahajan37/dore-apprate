f3b32e31e58e581e6d64f662f93bedf8f74082fb
Phodal HUANG <h@phodal.com>
2017-12-17 23:11:51 +0800

    [T] init projects

---
+++ ios/DoreAppRate/DoreAppRate.h
@@ -0,0 +1,12 @@
+//
+//  DoreAppRate.h
+//  DoreAppRate
+//
+//  Created by phodal on 17/12/2017.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DoreAppRate : NSObject
+
+@end
---
+++ ios/DoreAppRate/DoreAppRate.m
@@ -0,0 +1,12 @@
+//
+//  DoreAppRate.m
+//  DoreAppRate
+//
+//  Created by phodal on 17/12/2017.
+//
+
+#import "DoreAppRate.h"
+
+@implementation DoreAppRate
+
+@end
